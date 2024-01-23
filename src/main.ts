8955d539b9f570c5f70e39e90e540ade74b085f9
It21217654 <IT21217654@my.sliit.lk>
2024-01-23 23:40:56 +0530

    Arrays & Objects

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,73 @@
-let myName: string ='Rashini';
-let pass: number;
-let isloading:Boolean;
-let album: any;
-//let albm: string | number; union type
+let stringArr = ['one','hey','rash']
+
+let guitars = ['strat','les paul',1000]
+
+let mixedArr = [1,2,'three',true]
+
+stringArr[0]='john'
+stringArr.push('paul')
+
+guitars[0]=1967
+guitars.push('fender')
+
+let test =[]
+let bands: string[]=[]
+bands.push('Van helan')
+
+//tuple
+let myTuple : [string,number,boolean]=["dev", 45 , true]
+
+//objects
+let myObj: object
+myObj=[]
+console.log(typeof myObj)
+myObj=bands
+myObj={}
+
+const exampleObj={
+    prop1:'rash',
+    prop2: true,
+}
+exampleObj.prop1='John'
+
+type Guitarist={
+    name:string,
+    active?:boolean,
+    albms:(string | number)[]
+
+}
+
+let evh: Guitarist ={
+    name:'john',
+    active:true,
+    albms:[1789,2128,4773]
+
+}
+let jp: Guitarist ={
+    name:'john',
+    active:true,
+    albms:[1789,2128,4773]
+
+}
+
+// we can get now env and jp is eqaul bcz active is optional
+
+const greetGuitarist=(gitarist:Guitarist)=>{
+    if (greetGuitarist.name){
+    return 'Hello ${guitarist.name.toUpperCase()}!'
+}
+return 'Hellow'
+}
+
+console.log(greetGuitarist(jp))
 
 
-
-myName='Rashni';
-pass=123;
-isloading=true;
-album='My Album'; //any data type 
-
-const sum=(a: number, b:number)=> {
-    return a+b;
+//enum
+enum Role {ADMIN,READ_ONLY,AUTHOR}
+const person ={
+    name:'rash',
+    age:30,
+    hobbies:['sports','cooking'],
+    role:Role.ADMIN
 }
 
