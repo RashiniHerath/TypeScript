fb9a703d1bf069117cacf0a9d05f09a4ad75836f
It21217654 <IT21217654@my.sliit.lk>
2024-01-23 21:11:38 +0530

    Array & objects

---
+++ lesson3/src/main.ts
@@ -0,0 +1,18 @@
+let stringArr = ['one','hey','rash']
+
+let guitars = ['strat','les paul',1000]
+
+let mixedArr = [1,2,'three',true]
+
+stringArr[0]='john'
+stringArr.push('paul')
+
+guitars[0]=1967
+guitars.push('fender')
+
+let test =[]
+let bands: string[]=[]
+bands.push('Van helan')
+
+//tuple
+let myTuple : [string,number,boolean]=["dev", 45 , true]
