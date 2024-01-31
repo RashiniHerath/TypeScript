20a90f357596998b2037ad55bd2e668305ec6c89
Rashini Herath <99325024+RashiniHerath@users.noreply.github.com>
2024-01-31 14:42:01 +0530

    Lesson02

---
+++ Lesson02/src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let myName = 'Rashini';
+let pass;
+let isloading;
+let album;
+//let albm: string | number; union type
+myName = 'Rashni';
+pass = 123;
+isloading = true;
+album = 'My Album'; //any data type 
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ Lesson02/src/main.ts
@@ -0,0 +1,17 @@
+let myName: string ='Rashini';
+let pass: number;
+let isloading:Boolean;
+let album: any;
+//let albm: string | number; union type
+
+
+
+myName='Rashni';
+pass=123;
+isloading=true;
+album='My Album'; //any data type 
+
+const sum=(a: number, b:number)=> {
+    return a+b;
+}
+
